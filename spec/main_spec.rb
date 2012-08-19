4c6edd5be5696a10c3839b51b99e12f9f865c702
Ramon Torres <ramon.torres@edgebound.com>
2012-08-19 16:47:14 -0500

    initial import

---
+++ spec/main_spec.rb
@@ -0,0 +1,11 @@
+describe "Application 'current_address'" do
+
+  before do
+    @app = UIApplication.sharedApplication
+  end
+
+  it "has one window" do
+    @app.windows.size.should == 1
+  end
+
+end
